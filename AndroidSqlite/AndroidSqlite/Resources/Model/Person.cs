e7b00a5e11aefaacddabd498005c5e92f40c8c39
bekiryildirimm <bekir.yildirim.mdbf21@iste.edu.tr>
2023-04-18 15:49:28 +0300

    Ezan Vakti Mobil Uygulama

---
+++ AndroidSqlite/AndroidSqlite/Resources/Model/Person.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SQLite;
+namespace AndroidSqlite.Resources.Model
+{
+    [Table("Person")]
+    public class Person
+    {
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string City { get; set; }
+    }
+}
